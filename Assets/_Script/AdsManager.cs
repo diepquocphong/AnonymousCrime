9480933ff2e7ebdcd972815cf2405d5e10560f88
Phong <>
2024-12-27 15:45:18 +0700

    phong comit franklin michael

--- Assets/_Script/AdsManager.cs
+++ Assets/_Script/AdsManager.cs
@@ -28,7 +28,7 @@ public class AdsManager : MonoBehaviour
 
     public void ShowBanner()
     {
-        Gley.MobileAds.API.ShowBanner(Gley.MobileAds.BannerPosition.Bottom, Gley.MobileAds.BannerType.Adaptive);
+        Gley.MobileAds.API.ShowBanner(Gley.MobileAds.BannerPosition.Top, Gley.MobileAds.BannerType.Adaptive);
         OnAdShown("Banner");
     }
 
